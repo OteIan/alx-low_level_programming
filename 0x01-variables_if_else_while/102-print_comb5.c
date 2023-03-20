5198f1d04313051c80d9eb042fe350c0d3f73562
Ote Ian <oteian6@gmail.com>
2023-03-20 15:39:31 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,8 +23,8 @@ int main(void)
 			putchar((q % 10) + '0');
 			if (p == 98 && q == 99)
 				continue;
-				putchar(',');
-				putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
