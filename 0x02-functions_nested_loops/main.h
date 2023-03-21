1f028e42fb658b0d4276b9278180a3e69fea37d8
Ote Ian <oteian6@gmail.com>
2023-03-21 11:22:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+void printProgram(void)
+/*this prints '_putchar'*/
+{
+	printf("_putchar\n");
+}
+
+int main(void)
+{
+	printProgram();
+	return (0);
+}
