69ba6af86d1536827506a4aa47da603d5edb5b78
Ote Ian <oteian6@gmail.com>
2023-03-21 16:02:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @a: number to be added
+ * @b: number to be added
+ * Return: y is the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int y = a + b;
+
+	return (y);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: table of 9
+ */
+
+void times_table(void)
+{
+	
+}
