64d32fbd4abad35902cff8db2428ec736584c0d1
Ote Ian <oteian6@gmail.com>
2023-03-21 12:53:42 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for the lower case character
+ * main - main function
+ * Return: always 0
+ */
+
+int _islower(int c)
+{
+	if (c == [[*lower]])
+		_putchar('1');
+	else 
+		_putchar('0');
+}
