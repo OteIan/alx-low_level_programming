ff353ed274ae6b229720a767f8170ee78399b0bf
Ote Ian <oteian6@gmail.com>
2023-03-21 14:18:16 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,12 +11,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 	else if (n < 0)
+	{
 		return (-1);
 		_putchar('-');
+	}
 	else
+	{
 		_putchar(0);
 		return (0);
+	}
 }
