a856510c3e973b2fc839e618f5e5587da68949ed
Ote Ian <oteian6@gmail.com>
2023-03-21 15:41:11 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of a day
+ *
+ * Return: minutes of the day
+ */
+
+void jack_bauer(void)
+{
+	int m;
+	int h;
+
+	for (h = 0 ; h < 24 ; h++)
+	{
+		for (m = 0 ; m < 60 ; m++)
+		{
+			_putchar(h);
+			_putchar(':')
+			_putchar(m);
+		}
+	}
+}
