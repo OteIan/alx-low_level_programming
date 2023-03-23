2d2176af1643512b364e34090bbdee9662349a21
Ote Ian <oteian6@gmail.com>
2023-03-23 10:02:50 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: integer to be workeed on
+ *
+ * Return: 1 if c is uppercase otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		_putchar('1');
+	else
+		_putchar('0');
+	_putchar('\n');
+}
