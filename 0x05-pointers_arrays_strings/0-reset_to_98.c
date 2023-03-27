54e35fbd8b7d570c86a7ff4dbed3f33adf5cdde5
Ote Ian <oteian6@gmail.com>
2023-03-27 09:27:25 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the parameter to 98
+ * @n: parameter to be updated
+ * Return: always null
+ */
+
+void reset_to_98(int *n)
+{
+	int *ptr = &n;
+	*ptr = 98;
+	*n = *ptr;
+}
