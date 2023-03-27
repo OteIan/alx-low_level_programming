f568198d608e56180c91d84369b960cc9bfa362c
Ote Ian <oteian6@gmail.com>
2023-03-27 14:21:08 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the even character
+ * @str: string to be worked on
+ * Return: always null
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
