349b22f7a5efdff5a49724829c3dc003ff169958
Ote Ian <oteian6@gmail.com>
2023-03-27 16:02:14 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies and pastes the string pointed to
+ * @dest: where to paste the string
+ * @src: where to copy the string
+ * Return: always dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = *src;
+
+	*dest = i
+	return (dest);
+}
