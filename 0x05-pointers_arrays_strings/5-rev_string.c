ca193246fe2e21aa78e6b9125d47001011ef2f30
Ote Ian <oteian6@gmail.com>
2023-03-27 13:55:31 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: string to be printed
+ * Return: always null
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		count++;
+	}
+	count--;
+	for (i = count ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
