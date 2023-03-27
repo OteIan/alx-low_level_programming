0745ca6237f91cfe5bca38e746ac618d6ea7aa5c
Ote Ian <oteian6@gmail.com>
2023-03-27 12:13:33 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * Return: always null
+ */
+
+void print_rev(char *s);
+{
+	int i;
+
+	for (s[i] = '\0' ; i >= 0 ; i--)
+	{
+		if (s[i] != '\0')
+			_putchar(i);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
