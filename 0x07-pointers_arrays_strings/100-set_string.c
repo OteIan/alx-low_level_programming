94e0695418912643800ef0309ea7728844b6268b
Ote Ian <oteian6@gmail.com>
2023-04-04 14:44:54 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#inclide "main.h"
+
+/**
+ * set_string - sets pointer value to char
+ * @s: pointer to pointer
+ * @to: pointer to char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
