180662ad250a94947b763f8d61962eb44f8a9526
Ote Ian <oteian6@gmail.com>
2023-04-06 14:00:42 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	if (*s == 0)
-		return (1);
+		return (0);
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
@@ -24,7 +24,7 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (0);
+		return (1);
 	return (1 + _strlen_recursion(s + 1));
 }
 
