c63c3caa22982078e6c34b5ae58aeb3143bc888b
Ote Ian <oteian6@gmail.com>
2023-04-10 14:49:55 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - prints minimum no. of coins
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success and 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, out;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	result = 0;
+
+	if (i < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0 ; j < 5 && i >= 0 ; j++)
+	{
+		while (i >= coins[j])
+		{
+			result++;
+			i -= coins[j];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
