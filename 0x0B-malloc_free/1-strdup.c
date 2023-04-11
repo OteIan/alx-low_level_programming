b4340554dda2b63010bcc3b5b63dea28c0bd18f8
Ote Ian <oteian6@gmail.com>
2023-04-11 13:50:37 -0700

    char *_strdup(char *str) {         char *num;         int i;         int j = 0;
            if (str == NULL)
                    return (NULL);
    
            i = 0;
            while (str[i] != 0)
                    i++;
    
            num = malloc(sizeof(char) * (i + 1));
    
            if (num == NULL)
                    return (NULL);
    
            for (j = 0 ; str[j] ; j++)
                    num[j] = str[j];
            return (aaa);
    
    japihqgsgqysyqyqjoppl
    ]
    
    aiqjoio`9u`87854321`4578[]`

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to newly allocated space
+ * @str: string too be copied
+ *
+ * Return: NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *num;
+	int i;
+	int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	num = malloc(sizeof(char) * (i + 1));
+
+	if (num == NULL)
+		return (NULL);
+
+	for (j = 0 ; str[j] ; j++)
+		num[j] = str[j];
+	return (aaa);
+}
