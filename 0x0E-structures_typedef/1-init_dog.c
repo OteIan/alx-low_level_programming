8c6f4fbd676095ccda50deb00bc986581363096c
Ote Ian <oteian6@gmail.com>
2023-04-17 14:13:29 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * init_dog - initializes a variable of typr struct dog
+ * @d: pointer
+ * @name: name of user
+ * @age: age of user
+ * @owner: owner of file
+ * Return: always NULL
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
