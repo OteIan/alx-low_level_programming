bc76a635cbf758b94cb26092a5c0c26a58b4eb2c
Ote Ian <oteian6@gmail.com>
2023-04-21 11:07:52 -0700

    1-array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	if (size != 0 && int != NULL)
+	if (size != 0)
 	{
-	for (i = 0 ; i < size ; i++)
-	{
-		action(array[i]);
-	}
+		for (i = 0 ; i < size ; i++)
+			{
+				action(array[i]);
+			}
 	}
 }
