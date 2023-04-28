d2797a214f8ba995102a7e5492703c8a40435acc
Ote Ian <oteian9@gmail.com>
2023-04-28 14:34:20 -0700

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,5 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	return (0);
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
