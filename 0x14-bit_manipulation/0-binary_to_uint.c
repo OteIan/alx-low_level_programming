6a10e8ce04bc226e83818d9c62e1ee053f57e86f
Ote Ian <oteian9@gmail.com>
2023-05-04 12:01:14 -0700

    Conversion from binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,8 +7,8 @@
  * @b: pointer to a string of 0 and 1 chars
  *
  * Return: The converted number
- * 	Or 0 if there is one or more chars in the string b
- * 	that is not 0 or 1
+ * Or 0 if there is one or more chars in the string b
+ * that is not 0 or 1
  */
 
 unsigned int binary_to_uint(const char *b)
