f3e91b6b8bdfe52d23728de7fab203fee1172484
Ote Ian <oteian9@gmail.com>
2023-05-09 06:32:32 -0700

    Reads the content of a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,27 +3,29 @@
 /**
  * read_textfile - reads a text file
  *
- * @filename:
- * @letters:
+ * @filename: pointer to a string that contains the name of the file to read
+ * @letters: maximum number of letters to read
  *
- * Return: actual number of letters it could read and print
+ * Return: actual number of letters it could read and print, or 0 on failure
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	char *buffer;
-	ssize_t nwritten;
-	ssize_t nread;
+	FILE *fp; /*Pointer to a FILE object*/
+	char *buffer;/*Pointer to a block of memory that will hold 
+	the file contents*/
+	ssize_t nwritten;/*Number of bytes written to stdout*/
+	ssize_t nread;/*Number of bytes read from the file*/
 
-	if (!filename)
+	if (!filename) /*Check if filename is NULL*/
 		return (0);
 
-	fp = fopen(filename, "r");
-	if (!fp)
+	fp = fopen(filename, "r");/*Opens the file*/
+	if (!fp)/*Check if the file cannot be opened*/
 		return (0);
 
-	buffer = malloc(letters + 1);
+	buffer = (char *)malloc(letters + 1);
+	/*Allocates enough space for letters plus the null byte*/
 	if (!buffer)
 	{
 		fclose(fp);
@@ -31,7 +33,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	nread = fread(buffer, 1, letters, fp);
-	if (!nread)
+	/*Read data from the file into the buffer*/
+	if (nread == 0 && !feof(fp))/*Check if the read failed*/
 	{
 		free(buffer);
 		fclose(fp);
@@ -39,6 +42,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	nwritten = write(STDOUT_FILENO, buffer, nread);
+	/*Writes the buffer to stdout*/
 	if (nwritten < 0 || nwritten != nread)
 	{
 		free(buffer);
