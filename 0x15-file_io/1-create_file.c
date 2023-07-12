c91ac77f1be18f80859f79aa006c0e29b76d2688
Ote Ian <oteian9@gmail.com>
2023-07-12 05:42:28 -0700

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0, len;
-	ssize_t bytes_written = 0;
+	int fd = 0;
+	int len;/* Stores the length of text_content */
+	ssize_t bytes_written = 0;/* Stores no. of bytes written to the file */
 
+	/* Checks if 'filename' is a NULL pointer */
 	if (filename == NULL)
 		return (-1);
 
+	/* Checks if text_content is not a NULL pointer*/
 	if (text_content != NULL)
 	{
 		for (len = 0 ; text_content[len] != '\0' ; len++)
 			continue;
 	}
 
+	/* Opens the file as per the instructions */
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	/* Checks if the 'open' call failed */
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
+	/* Writes 'len' number of bytes from 'text_content' to file 'fd' */
 		bytes_written = write(fd, text_content, len);
 		if (bytes_written == -1)
 		{
