cbef626f33b1b6c472683261736119ce9f08a0e8
Ote Ian <oteian9@gmail.com>
2023-07-12 05:35:02 -0700

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0, len = 0, i;
+	int fd = 0, len;
 	ssize_t bytes_written = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	for (i = 0 ; text_content[i] != '\0' ; i++)
-		len++;
+	if (text_content != NULL)
+	{
+		for (len = 0 ; text_content[len] != '\0' ; len++)
+			continue;
+	}
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
