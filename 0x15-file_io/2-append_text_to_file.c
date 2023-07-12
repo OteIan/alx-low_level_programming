267bd4bd9987ec016a158282a863efab1dc88928
Ote Ian <oteian9@gmail.com>
2023-07-12 06:32:08 -0700

    Appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,25 +10,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = 0;
-	int len;
-	ssize_t bytes_written = 0;
+	int fd = 0;/* Stores the file descriptor */
+	int len;/* Stores the lrngth of 'text_content' */
+	ssize_t bytes_written;/* Stores the no. of bytes written to the file */
 
+	/* Checks if 'filename' is a NULL pointer */
 	if (filename == NULL)
 		return (-1);
 
+	/* Checks if 'text_content' is not a NULL pointer */
 	if (text_content != NULL)
 	{
 		for (len = 0 ; text_content[len] != '\0' ; len++)
 			continue;
 	}
 
+	/* Opens the file as read only flag with append flag */
 	fd = open(filename, O_WRONLY | O_APPEND);
+	/* Checks if the open call failed */
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
+	/* Writes 'len' number of bytes from 'text_content' to file 'fd' */
 		bytes_written = write(fd, text_content, len);
 		if (bytes_written == -1)
 		{
