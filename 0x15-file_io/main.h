2d723a7dce918262f70bc9ddeef96ce98b2e8e88
Ote Ian <oteian9@gmail.com>
2023-07-14 13:41:54 -0700

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <elf.h>
 
 /* Prototypes of functions */
 int _putchar(char c);
@@ -15,5 +16,18 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
+
+/* 100-elf_header.c */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_os_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_type, unsigned char *e_ident);
+void close_elf(int elf);
+
 
 #endif
