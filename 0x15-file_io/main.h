bc9d6b7df76bcce3e8222c0cf1010b88ec2a98a3
Ote Ian <oteian9@gmail.com>
2023-07-28 09:31:02 -0700

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,6 +16,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
+void _close(int fd);
+char *create_buffer(char *name);
 
 /* 100-elf_header.c */
 void check_elf(unsigned char *e_ident);
