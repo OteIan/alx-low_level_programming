4a000a032045f70ff6ba11cadafcf941220cf488
Ote Ian <oteian9@gmail.com>
2023-09-09 00:36:58 +0300

    Add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,39 @@
 #include "hash_tables.h"
 
 /**
+ * make_hash_node - Makes a new hash node
+ * 
+ * @key: Pointer to the key of the node
+ * @value: Pointer to the value of the node
+ * 
+ * Return: Pointer to the new hash node, or NULL on failure
+*/
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+    hash_node_t *new_node;
+
+    new_node = malloc(sizeof(hash_node_t));
+
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->value);
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->next = NULL;
+    return (new_node);
+}
+
+/**
  * hash_table_set - This adds an element to the hash table
  * 
  * @ht: Pointer to the hash table to add/update the value to
@@ -12,28 +45,20 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
-    hash_node_t *current_item, *new_item = malloc(sizeof(hash_node_t));
+    hash_node_t *current_item, *new_item;
 
-    if (key == NULL || ht == NULL || new_item == NULL)
+    if (ht == NULL || ht->array == NULL || ht->size == NULL ||
+        key == NULL || strlen(key) == 0 || value == NULL)
         return (0);
-    index = key_index(key, ht->size);
+
+    new_item = malloc(sizeof(hash_node_t));
+    new_item = make_hash_node(key, value);
+    index = key_index(key, ht->size); 
     current_item = ht->array[index];
-    new_item->key = strdup(key);
-    if (new_item->key == NULL)
-    {
-        free(new_item);
-        return (0);
-    }
-    new_item->value = strdup(value);
-    if (new_item->value == NULL)
-    {
-        free(new_item->key);
-        free(new_item);
-        return (0);
-    }
-    new_item->next = NULL;
+
     if (current_item == NULL)
         ht->array[index] = new_item;
+    
     else
     {
         while (current_item)
