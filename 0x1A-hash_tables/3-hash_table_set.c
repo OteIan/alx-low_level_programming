145bdd6a04136fc059a93043d737e4f4f7c2a64d
Ote Ian <oteian9@gmail.com>
2023-09-09 00:45:15 +0300

    Add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,30 +7,30 @@
  * @value: Pointer to the value of the node
  * 
  * Return: Pointer to the new hash node, or NULL on failure
-*/
+ */
 hash_node_t *make_hash_node(const char *key, const char *value)
 {
-    hash_node_t *new_node;
+	hash_node_t *new_node;
 
-    new_node = malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t));
 
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->value);
-        free(new_node);
-        return (NULL);
-    }
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->value);
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = NULL;
-    return (new_node);
+	new_node->next = NULL;
+	return (new_node);
 }
 
 /**
@@ -41,40 +41,40 @@ hash_node_t *make_hash_node(const char *key, const char *value)
  * @value: Pointer to the value associated with the key
  * 
  * Return: 1 if succeded, 0 otherwise
-*/
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *current_item, *new_item;
+	unsigned long int index;
+	hash_node_t *current_item, *new_item = malloc(sizeof(hash_node_t));
 
-    if (ht == NULL || ht->array == NULL || ht->size == NULL ||
-        key == NULL || strlen(key) == 0 || value == NULL)
-        return (0);
+	if (ht == NULL || ht->array == NULL || ht->size == NULL ||
+			key == NULL || strlen(key) == 0 || value == NULL ||
+			new_item == NULL)
+		return (0);
 
-    new_item = malloc(sizeof(hash_node_t));
-    new_item = make_hash_node(key, value);
-    index = key_index(key, ht->size); 
-    current_item = ht->array[index];
+	new_item = make_hash_node(key, value);
+	index = key_index(key, ht->size); 
+	current_item = ht->array[index];
 
-    if (current_item == NULL)
-        ht->array[index] = new_item;
-    
-    else
-    {
-        while (current_item)
-        {
-            if (strcmp(current_item->key, key) == 0)
-            {
-                free(current_item->value);
-                current_item->value = strdup(value);
-                if (current_item->value == NULL)
-                    return (0);
-                return (1);
-            }
-            current_item = current_item->next;
-        }
-        new_item->next = ht->array[index];
-        ht->array[index] = new_item;
-    }
-    return (1);
+	if (current_item == NULL)
+		ht->array[index] = new_item;
+
+	else
+	{
+		while (current_item)
+		{
+			if (strcmp(current_item->key, key) == 0)
+			{
+				free(current_item->value);
+				current_item->value = strdup(value);
+				if (current_item->value == NULL)
+					return (0);
+				return (1);
+			}
+			current_item = current_item->next;
+		}
+		new_item->next = ht->array[index];
+		ht->array[index] = new_item;
+	}
+	return (1);
 }
