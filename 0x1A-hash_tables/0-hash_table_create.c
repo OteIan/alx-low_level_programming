dfe4e8614578a73a6a29a6c9378a34f8af508d29
OteIan <oteian9@gmail.com>
2023-09-08 03:20:32 -0700

    Create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,10 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-	{
-		free(table);
 		return (NULL);
-	}
 
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
