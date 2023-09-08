1b92f4813b65e62dde0e9c1903411517301f7ca9
Ote Ian <oteian9@gmail.com>
2023-09-08 12:56:55 +0300

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,28 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *table;
+    
+    if (size == 0)
+        return NULL;
+
+    table = (hash_table_t *)malloc(sizeof(hash_table_t));
+    if (table == NULL)
+    {
+        free(table);
+        return (NULL);
+    }
+
+
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t **));
+    if (table->array == NULL)
+    {
+        free(table);
+        return NULL;
+    }
 
 	table->size = size;
-	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t **));
-
-	for (i = 0 ; i < table->size ; i++)
+    for (i = 0 ; i < table->size ; i++)
 		table->array[i] = NULL;
 
 	return (table);
