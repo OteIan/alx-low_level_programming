4afd37c2b7924e9017ba1972b9dfce2c739efffe
Ote Ian <oteian9@gmail.com>
2023-09-10 13:26:01 -0700

    Retrieve value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,27 +2,31 @@
 
 /**
  * hash_table_get - Retrieves a value associated with key
- * 
+ *
  * @ht: Pointer to the hash table to look into
  * @key: Pointer to the key to look for
- * 
+ *
  * Return: value associated with the element or
  * NULL if key cannot be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current_item;
-	int i;
+	int index = 0;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-			key == NULL || len(key) == 0)
+			key == NULL || strlen(key) == 0)
 		return (NULL);
 
-	for (i = 0 ; ht->array[i] != NULL ; i++)
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current_item = ht->array[index];
+
+	while (current_item != NULL)
 	{
-		current_item = ht->array[i];
 		if (strcmp(current_item->key, key) == 0)
 			return (current_item->value);
+		current_item = current_item->next;
 	}
 
 	return (NULL);
