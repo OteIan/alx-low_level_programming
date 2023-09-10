7783d726aeac5c0c5bc9220e49281d4c1662c01f
Ote Ian <oteian9@gmail.com>
2023-09-10 23:58:14 +0300

    Print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ *
+ * @ht: Pointer to the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i, count = 0, itr = 0;
+	const hash_table_t *temp = ht;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	for (i = 0 ; i < temp->size ; i++)
+	{
+		if (ht->array[i] != NULL)
+			count++;
+	}
+
+	printf("{");
+	for (i = 0 ; i < ht->size ; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			if (itr != count - 1)
+				printf(", ");
+			itr++;
+		}
+	}
+	printf("}\n");
+	return;
+}
