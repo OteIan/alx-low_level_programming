141821efa8e71ff37c2b5d1ba0aa3a89e61b317f
Ote Ian <oteian9@gmail.com>
2023-09-10 15:12:35 -0700

    Delete hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,8 +7,8 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
 	hash_node_t *node, *temp;
+	hash_table_t *head = ht;
 	unsigned long int i;
 
 	if (ht->array == NULL)
@@ -22,7 +22,7 @@ void hash_table_delete(hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			node = ht->array[i];
-			while (node)
+			while (node != NULL)
 			{
 				temp = node->next;
 				free(node->key);
