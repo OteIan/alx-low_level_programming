66bec02b0764419594d1a35eab44833bceb6e5d6
Ote Ian <oteian9@gmail.com>
2023-10-25 14:20:19 +0300

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Island perimeter module definition"""
+
+
+def island_perimeter(grid):
+    """Function that returns the perimeter of the island described in 'grid'"""
+    length_row = len(grid)
+    length_column = len(grid[1])
+
+    assumed_Perimeter = 0
+    connections = 0
+    actual_Perimeter = 0
+
+    for i in range(0, length_row):
+        for j in range(0, length_column):
+
+            if grid[i][j] == 1:
+                assumed_Perimeter += 4
+
+                # Top Check
+                if i != 0 and grid[i - 1][j] == 1:
+                    connections += 2
+
+                # Side check
+                if j != 0 and grid[i][j - 1] == 1:
+                    connections += 2
+    
+    actual_Perimeter = assumed_Perimeter - connections
+
+    return actual_Perimeter
