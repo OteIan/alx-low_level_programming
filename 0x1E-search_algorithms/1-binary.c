385cb775cba8b701cd2befcf2889431014d4c780
Ote Ian <oteian9@gmail.com>
2023-12-12 05:44:59 +0300

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,16 +1,48 @@
 #include "search_algos.h"
 
 /**
+ * recursive_findElement - Recursive function that searches for a value
+ * 
+ * @start: Starting index to begin the search
+ * @end: Ending index to end the search
+ * @value: Value being searched for
+ * 
+ * Return: The matching element
+*/
+int recursive_findElement(int *array, int start, int end, int value)
+{
+    int middle = (start + end) / 2, i;
+
+    printf("Searching in array: %d", array[start]);
+    for (i = start + 1 ; i < end; i++)
+        printf(", %d", array[i]);
+
+    if (start > end)
+        return (-1);
+    else if (array[middle] == value)
+        return (middle);
+    else if (array[middle] > value)
+        return (recursive_findElement(array, start, middle - 1, value));
+    else if (array[middle] < value)
+        return (recursive_findElement(array, middle + 1, end, value));
+}
+
+/**
  * binary_search - Searches for a value in an array of integers using the 
  * binary search algorithm
- * 
+ *
  * @array: pointer to the first element of the array to search in
  * @size: the number of elements in array
  * @value: value to search for
- * 
+ *
  * Return: First index where value is located otherwise -1
-*/
+ */
 int binary_search(int *array, size_t size, int value)
 {
+    size_t i;
 
+    if (array == NULL)
+        return (-1);
+
+    return recursive_findElement(array, 0, size - 1, value);
 }
