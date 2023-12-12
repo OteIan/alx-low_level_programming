214d7ca93c504ee20c794477492afe9c438e2de0
Ote Ian <oteian9@gmail.com>
2023-12-12 05:17:44 +0000

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,33 +2,38 @@
 
 /**
  * jump_search - Searches for a value in a sorted array using jump search
- * 
+ *
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: Vlue to be searched for
- * 
+ *
  * Return: First index where vlue is located
-*/
+ */
 int jump_search(int *array, size_t size, int value)
 {
-    int i, start = 0, end = sqrt(size);
+	int i;
+	size_t start = 0, end = sqrt(size);
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while(array[end] < value && start < size)
-    {
-        start = end;
-        end += sqrt(size);
-        if (end > size - 1)
-            end = size;
-    }
+	while (array[end] < value && start < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+		start = end;
+		end += sqrt(size);
+		if (end > size - 1)
+			end = size;
+	}
 
-    for (i = 0 ; i < end ; i++)
-    {
-        if (array[i] == value)
-            return (i);
-    }
+	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 
-    return (-1);
+	for (i = start ; (size_t)i <= end ; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
