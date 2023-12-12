d7951450693471e147c10ea8cf072477dec88434
Ote Ian <oteian9@gmail.com>
2023-12-12 03:07:06 +0000

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,33 +2,37 @@
 
 /**
  * recursive_findElement - Recursive function that searches for a value
- * 
+ *
+ * @array: Pointer to the array being searched
  * @start: Starting index to begin the search
  * @end: Ending index to end the search
  * @value: Value being searched for
- * 
+ *
  * Return: The matching element
-*/
+ */
 int recursive_findElement(int *array, int start, int end, int value)
 {
-    int middle = (start + end) / 2, i;
+	int middle = (start + end) / 2, i;
 
-    printf("Searching in array: %d", array[start]);
-    for (i = start + 1 ; i < end; i++)
-        printf(", %d", array[i]);
+	printf("Searching in array: %d", array[start]);
+	for (i = start + 1 ; i <= end; i++)
+		printf(", %d", array[i]);
+	printf("\n");
 
-    if (start > end)
-        return (-1);
-    else if (array[middle] == value)
-        return (middle);
-    else if (array[middle] > value)
-        return (recursive_findElement(array, start, middle - 1, value));
-    else if (array[middle] < value)
-        return (recursive_findElement(array, middle + 1, end, value));
+	if (start >= end)
+		return (-1);
+	else if (array[middle] == value)
+		return (middle);
+	else if (array[middle] > value)
+		return (recursive_findElement(array, start, middle - 1, value));
+	else if (array[middle] < value)
+		return (recursive_findElement(array, middle + 1, end, value));
+
+	return (-1);
 }
 
 /**
- * binary_search - Searches for a value in an array of integers using the 
+ * binary_search - Searches for a value in an array of integers using the
  * binary search algorithm
  *
  * @array: pointer to the first element of the array to search in
@@ -39,10 +43,8 @@ int recursive_findElement(int *array, int start, int end, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i;
+	if (array == NULL)
+		return (-1);
 
-    if (array == NULL)
-        return (-1);
-
-    return recursive_findElement(array, 0, size - 1, value);
+	return (recursive_findElement(array, 0, size - 1, value));
 }
